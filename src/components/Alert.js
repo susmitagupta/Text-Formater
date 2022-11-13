e3e00e71c289da7c00f447d7813c3eb1cc2bf337
Susmita <guptasusmita24@gmail.com>
2022-11-13 22:36:09 +0530

    edited app and index

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,7 +4,7 @@ export default function Alert(props) {
   return (
    
       
-    // props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`}  role="alert">
+    
      props.alert &&  <div className="alert alert-warning alert-dismissible fade show" role="alert">
      <strong>{props.alert.type}</strong>: {props.alert.msg}
   <button type="button" className="close" data-dismiss="alert" aria-label="Close">
