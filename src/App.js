1ca5db61d5c62d61317ef9a233cccc550a3d5b6f
Susmita <guptasusmita24@gmail.com>
2022-11-14 14:59:59 +0530

    done editing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import About from "./components/About";
+import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
 
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState("light");
-  
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "rgb(4 33 62)";
       showAlert("Dark Mode Enabled", "Success");
-      
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light Mode Enabled", "Success");
-     
     }
   };
 
@@ -34,14 +32,29 @@ function App() {
     setAlert(null);
   }, 2000);
 
-
-
   return (
     <>
-    <Navbar title="Text Editor"  about="About Us"  mode={mode}  toggleMode={toggleMode}/>
-    <Alert alert={alert} />
-    <TextForm title="Enter the text to analize" mode={mode}  showAlert={showAlert}/>
-    {/* <About/> */}
+      <Router>
+        <Navbar
+          title="Text Editor"
+          about="About Us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+
+        <div className="container">
+          <Routes>
+
+             <Route path="/" element={<TextForm title="Enter the text to analize" mode={mode} showAlert={showAlert} />} />
+           
+            <Route path="/about" element={<About />} />
+
+           
+
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 
 export default function Navbar(props) {
@@ -24,14 +25,14 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/">
                 Home <span className="sr-only">(current)</span>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.about}
-              </a>
+              </Link>
             </li>
           </ul>
           
